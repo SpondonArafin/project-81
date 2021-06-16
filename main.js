0be57b22765ca24b2502e93e8842e44ce7e4122b
SpondonArafin <82678481+SpondonArafin@users.noreply.github.com>
2021-06-15 21:33:40 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+
+ctx.beginPath();
+ctx.strokeStyle("grey");
+ctx.lineWidth = 4;
+ctx.arc(250,210,40,0,2*Math.PI);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle("blue");
+ctx.lineWidth = 4;
+ctx.arc(250,210,40,0,2*Math.PI);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle("black");
+ctx.lineWidth = 4;
+ctx.arc(300,260,40,0,2*Math.PI);
+ctx.stroke();
